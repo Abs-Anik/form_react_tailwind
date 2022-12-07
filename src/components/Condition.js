0c389346bbc29dd442c073f029e74601151f202d
Md. Abu Bakkar Siddik <abubakkarsiddik.cse@gmail.com>
2022-12-07 10:54:19 +0600

    add responsive

--- src/components/Condition.js
+++ src/components/Condition.js
@@ -1,11 +1,12 @@
 import "../App.css";
+import '../style.css';
 function Condition() {
   return (
     <>
-      <p className="text-left text-slate-50 mt-2">
+      <p className="text-left text-slate-50 mt-2 custom-res-sm">
         <small>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla
-          aperiam porro tenetur eum quibusdam{" "}
+          aperiam porro tenetur eum quibusdam
           <span class="underline underline-offset-1">asrepellat</span>
         </small>
       </p>
@@ -17,21 +18,21 @@ function Condition() {
             type="checkbox"
           />
           <label
-            className="form-check-label inline-block text-white"
+            className="form-check-label inline-block text-white custom-res"
             for="flexCheckDefault"
           >
             <small>
-              {" "}
-              * Lorem ipsum dolor sit amet consectetur{" "}
+              
+              * Lorem ipsum dolor sit amet consectetur
               <span className="underline underline-offset-1">
                 adipisicing
-              </span>{" "}
+              </span>
               elit
             </small>
           </label>
         </div>
       </div>
-      <p className="text-start ml-9 text-sm text-white underline underline-offset-1">
+      <p className="text-start ml-9 text-sm text-white underline underline-offset-1 custom-res-p">
         Lorem ipsum dolor sit amet.
       </p>
 
@@ -42,16 +43,16 @@ function Condition() {
             type="checkbox"
           />
           <label
-            className="form-check-label inline-block text-white"
+            className="form-check-label inline-block text-white custom-res"
             for="flexCheckDefault"
           >
             <small className="ml-3">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.{" "}
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
             </small>
           </label>
         </div>
       </div>
-      <p className="text-start ml-9 text-sm text-white">
+      <p className="text-start ml-9 text-sm text-white custom-res-p">
         Perspiciatis laborum esse qui deserunt quas aperiam quisquam hic
         repudiandae illum
       </p>
